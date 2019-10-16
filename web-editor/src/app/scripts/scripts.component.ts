41b41cb315e64bc3f251c230c9fb5d0296e2789d
GiacomoCortesi <gcortesi@jmawireless.com>
2019-10-16 19:03:45 +0200

    fix

--- web-editor/src/app/scripts/scripts.component.ts
+++ web-editor/src/app/scripts/scripts.component.ts
@@ -17,7 +17,7 @@ export class ScriptsComponent implements OnInit {
   ngOnInit() {
     this.data.getScriptsPath().subscribe(
       data => {
-        this.path = JSON.stringify(data)
+        this.path = <string><unknown>data
         this.showFiles(this.path);
         this.getFiles(this.path)
       }
